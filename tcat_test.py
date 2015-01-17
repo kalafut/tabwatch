4c278af320d4207180967d8531185adb7d1d9baf
Jim Kalafut <jim@kalafut.net>
2015-01-17 15:39:17 -0800

    Got stuff under test
    Fixes #5.

---
+++ tcat_test.py
@@ -0,0 +1,11 @@
+import unittest
+import tcat
+
+class TestSequenceFunctions(unittest.TestCase):
+    def test_config(self):
+        config = tcat.loadconfig("test.json")
+        self.assertEquals(config["colors"], { "green": "#0ceb43", "red": "#f5384b", "yellow": "#dff51d" })
+
+
+if __name__ == '__main__':
+    unittest.main()
