c9c24aab8d409139570c07a410fcc3c07adc13a5
Jim Kalafut <jim@kalafut.net>
2015-01-18 13:22:07 -0800

    Read more from config

--- tcat_test.py
+++ tcat_test.py
@@ -3,7 +3,7 @@ import tcat
 
 class TestSequenceFunctions(unittest.TestCase):
     def test_config(self):
-        config = tcat.loadconfig("test.yaml")
+        config = tcat.loadconfig("test.json")
         self.assertEquals(config["colors"], { "green": "0ceb43", "red": "f5384b", "yellow": "dff51d" })
 
 if __name__ == '__main__':
